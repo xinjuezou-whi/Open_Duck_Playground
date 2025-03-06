7f9136792a3bc2c22ac882ab520b1a6c8dc99ce2
apirrone <awanegenebayo@gmail.com>
2025-03-06 12:48:36 +0100

    backlash in obs

--- playground/open_duck_mini_v2/base.py
+++ playground/open_duck_mini_v2/base.py
@@ -121,7 +121,7 @@ class OpenDuckMiniV2Env(mjx_env.MjxEnv):
         self.backlash_idx_to_add = []
 
         for i, actuator_name in enumerate(self.actuator_names):
-            if actuator_name + "_backlash" not in self.backlash_joint_names
+            if actuator_name + "_backlash" not in self.backlash_joint_names:
                 self.backlash_idx_to_add.append(i)
 
         print(f"actuators: {self.actuator_names}")
