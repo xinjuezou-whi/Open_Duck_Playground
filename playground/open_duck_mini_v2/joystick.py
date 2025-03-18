79b05d7088b8750d0524b0d10313e194240f62c9
apirrone <awanegenebayo@gmail.com>
2025-03-18 10:06:52 +0100

    update

--- playground/open_duck_mini_v2/joystick.py
+++ playground/open_duck_mini_v2/joystick.py
@@ -81,11 +81,11 @@ def default_config() -> config_dict.ConfigDict:
                 # orientation=-0.5,
                 torques=-1.0e-3,
                 # action_rate=-0.375,  # was -1.5
-                action_rate=-0.5,  # was -1.5
+                action_rate=-0.6,  # was -1.5
                 stand_still=-0.3,  # was -1.0 TODO try to relax this a bit ?
                 alive=20.0,
                 imitation=1.0,
-                head_pos=-0.3,
+                head_pos=-1.0,
             ),
             tracking_sigma=0.01,  # was working at 0.01
         ),
