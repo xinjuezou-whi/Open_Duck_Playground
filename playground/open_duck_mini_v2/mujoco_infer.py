792258e26312281dff9e3c77b5597718c9a4a93b
apirrone <awanegenebayo@gmail.com>
2025-03-22 13:41:18 +0100

    action scale 1.0

--- playground/open_duck_mini_v2/mujoco_infer.py
+++ playground/open_duck_mini_v2/mujoco_infer.py
@@ -31,7 +31,7 @@ class MjInfer:
         self.angularVelocityScale = 1.0
         self.dof_pos_scale = 1.0
         self.dof_vel_scale = 0.05
-        self.action_scale = 0.25
+        self.action_scale = 1.0
 
         self.action_filter = LowPassActionFilter(50, cutoff_frequency=37.5)
 
