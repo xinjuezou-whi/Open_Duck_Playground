c80ecd5d274cb7d7626de8956751a231bd57cf5a
apirrone <awanegenebayo@gmail.com>
2025-03-22 17:00:34 +0100

    trying stuff

--- playground/open_duck_mini_v2/joystick.py
+++ playground/open_duck_mini_v2/joystick.py
@@ -81,11 +81,11 @@ def default_config() -> config_dict.ConfigDict:
         reward_config=config_dict.create(
             scales=config_dict.create(
                 tracking_lin_vel=2.5,
-                tracking_ang_vel=4.0,
+                tracking_ang_vel=8.0,
                 # orientation=-0.5,
                 torques=-1.0e-3,
                 # action_rate=-0.375,  # was -1.5
-                action_rate=-0.3,  # was -1.5
+                action_rate=-0.2,  # was -1.5
                 stand_still=-0.3,  # was -1.0 TODO try to relax this a bit ?
                 alive=20.0,
                 imitation=1.0,
